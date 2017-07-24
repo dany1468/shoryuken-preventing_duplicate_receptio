cf72d80255c2ccd8203b808ca724c98c18e69943
dany1468 <dany1468@gmail.com>
2017-07-24 15:47:00 +0900

    Convert to single quoted and Add frozen_string_literal comment

--- lib/shoryuken/preventing_duplicate_reception.rb
+++ lib/shoryuken/preventing_duplicate_reception.rb
@@ -1,4 +1,6 @@
-require "shoryuken/preventing_duplicate_reception/version"
+# frozen_string_literal: true
+
+require 'shoryuken/preventing_duplicate_reception/version'
 
 module Shoryuken
   module PreventingDuplicateReception
