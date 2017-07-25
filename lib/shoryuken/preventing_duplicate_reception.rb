1c95a6357673992f6d5a65efbb50f6c960cf9d6e
dany1468 <dany1468@gmail.com>
2017-07-25 17:58:45 +0900

    require related modules

--- lib/shoryuken/preventing_duplicate_reception.rb
+++ lib/shoryuken/preventing_duplicate_reception.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'shoryuken/preventing_duplicate_reception/dynamodb_lock'
 require 'shoryuken/preventing_duplicate_reception/version'
+require 'shoryuken/middleware/server/duplicate_reception_preventer'
 
 module Shoryuken
   module PreventingDuplicateReception
